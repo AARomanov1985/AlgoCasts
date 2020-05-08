c4e1c343513c384998862bb2d7d272109e8331e1
Aleksandr Romanov <AARomanov1985@yandex.ru>
2020-05-08 10:13:58 +0300

    reversestring

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,34 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    let result = "";
+    for (let i = str.length - 1; i >= 0; i--) {
+        result += str.charAt(i);
+    }
+    return result;
+}
+
+function reverseByUsingArray(str) {
+    let arr = str.split('');
+    arr.reverse();
+    return arr.join('');
+}
+
+function reverseByUsingArrayShort(str) {
+    return str.split('').reverse().join('');
+}
+
+function reverseByUsingForEach(str) {
+    let reversed = "";
+    for (let c of str) {
+        reversed += c;
+    }
+    return reversed;
+}
+
+function reverseByUsingReduce(str) {
+    return str.split('').reduce((char, rev) => char + rev, '');
+}
 
 module.exports = reverse;
